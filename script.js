26bdb0a818e5d93a3a5a6285e8b643122d4842a2
Adrian-Koh <adriankoh216@yahoo.com>
2025-05-14 17:03:28 +0800

    Add clear buttons and operate functions

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelector('.buttons-container');
 
+let a = NaN;
+let b = NaN;
+let operation;
+
 buttons.addEventListener('click', (event) => {
     let target = event.target;
+    let result;
+    switch (target.class) {
+        case 'number':
+            if (Number.isNaN(a)) {
+                a = +target.textContent;
+            }
+            else if (Number.isNaN(b)) {
+                b = +target.textContent;
+            }
+            else {
+                // a and b already 
+            }
+
+            break;
+        case 'operator':
+            if (operation !== undefined) {
+                // throw error
+            }
+            switch (target.textContent) {
+                case '+':
+
+            }
+            break;
+        case 'equals':
+            break;
+    }
+
     display.textContent += target.textContent;
 });
+
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
